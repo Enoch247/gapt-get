c27fc12b5e86c9c1778975c5abb522bafd1e844f
Joshua DeWeese <josh.deweese@gmail.com>
2013-07-15 09:23:36 -0400

    added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#*******************************************************************************
+# Written by: Joshua DeWeese(josh.deweese@gmail.com)
+# please catch and release
+#*******************************************************************************
+
+PROJDIR=`dirname $0`
+PREFIX="$HOME/bin"
+
+install $PROJDIR/gapt-* $PREFIX
